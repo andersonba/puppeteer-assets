288decbd945b410ee0ed41f52bddd4abe92f1b0b
Anderson de Oliveira <andersonba@me.com>
2018-03-25 13:45:41 -0300

    fix docker push script

--- resources/docker_push.sh
+++ resources/docker_push.sh
@@ -3,7 +3,7 @@
 REPO="andersonba/prometheus-assets"
 VERSION=$(cat package.json |json version)
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -f prometheus/Dockerfile -t $REPO .
 docker tag $REPO "$REPO:$VERSION"
 docker push $REPO
